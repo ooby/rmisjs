f8723234e9da60523c5f6bb9f9e2d61460e5f8c8
raviolin <raviolin@icloud.com>
2017-12-04 16:22:37 +0900

    0.4.21

--- composer/mongo/sync/timeslots.js
+++ composer/mongo/sync/timeslots.js
@@ -3,112 +3,105 @@ const TimeSlot = require('../model/timeslot');
 const Location = require('../model/location');
 const moment = require('moment');
 
-module.exports = async(rmis, clinicId) => {
-    let promises = [];
-    let dates = createDates();
-    let [appointmentService, locations] = await Promise.all([
-        rmis.appointment(),
-        Location.find().distinct('_id').exec()
-    ]);
+const toMidnight = dateString => moment(dateString + 'T00:00:00.000' + moment().format('Z')).toDate();
+
+const update = async(date, location, organization, appointmentService) => {
+    let midnight = toMidnight(date);
+    let times = await appointmentService.getTimes({
+        location,
+        date
+    });
+    times = (
+        (!!times.interval.timePeriod ? times.interval.timePeriod : [])
+        .map(i => {
+            return {
+                from: new Date(`${date}T${i.from}`),
+                to: new Date(`${date}T${i.to}`),
+                date: midnight,
+                location,
+                unavailable: i.notAvailableSources ? i.notAvailableSources.notAvailableSource.map(i => i.source) : [],
+                services: i.availableServices ? i.availableServices.service : []
+            };
+        })
+    );
+    let tmp = await appointmentService.getReserveFiltered({
+        date,
+        organization,
+        location
+    });
+    times = (
+        (times || [])
+        .concat(
+            ((tmp || []).slot || [])
+            .filter(i => !!i.timePeriod.to)
+            .map(i => {
+                return {
+                    from: new Date(`${date}T${i.timePeriod.from}`),
+                    to: new Date(`${date}T${i.timePeriod.to}`),
+                    date: midnight,
+                    location,
+                    status: i.status
+                };
+            })
+        )
+    );
+    let froms = times.map(i => i.from);
     await TimeSlot.remove({
-        $or: [{
-                location: {
-                    $nin: locations
-                }
-            },
+        location,
+        date: midnight,
+        from: {
+            $nin: froms
+        }
+    }).exec();
+    let existing = await Location.distinct('from', {
+        date: midnight,
+        location
+    }).lean().exec();
+    existing = existing.map(i => i.valueOf());
+    for (let time of times) {
+        try {
+            let from = time.from.valueOf();
+            if (existing.indexOf(from) < 0) {
+                await new TimeSlot(time).save();
+                existing.push(from);
+            } else {
+                await TimeSlot.update({
+                    from: time.from,
+                    location
+                }, {
+                    $set: {
+                        to: time.to,
+                        status: time.status
+                    }
+                }).exec();
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+};
+
+module.exports = async(rmis, clinicId) => {
+    let appointmentService = await rmis.appointment();
+    let dates = createDates();
+    let locs = await Location.distinct('_id').exec();
+    await TimeSlot.remove({
+        $or: [
             {
                 date: {
                     $nin: dates
                 }
+            },
+            {
+                location: {
+                    $nin: locs
+                }
             }
         ]
     }).exec();
-    for (let location of locations) {
+    for (let loc of locs) {
         for (let date of dates) {
-            let froms = [];
-            let midnight = moment(date + 'T00:00:00.000' + moment().format('Z')).toDate();
-            let [existing, interval] = await Promise.all([
-                TimeSlot.distinct('from', {
-                    location,
-                    date: midnight
-                }).exec(),
-                appointmentService.getTimes({
-                    location,
-                    date
-                })
-            ]);
-            existing = existing.map(i => i.valueOf());
-            interval = interval || {};
-            interval = !!interval.interval.timePeriod ? interval.interval.timePeriod : [];
-            for (let slot of interval) {
-                let from = new Date(`${date}T${slot.from}`);
-                let fromNum = from.valueOf();
-                froms.push(fromNum);
-                if (existing.indexOf(fromNum) > -1) continue;
-                existing.push(fromNum);
-                let to = new Date(`${date}T${slot.to}`);
-                let unavailable = !!slot.notAvailableSources ? slot.notAvailableSources.notAvailableSource : [];
-                unavailable = unavailable.map(i => i.source);
-                let services = !!slot.availableServices ? slot.availableServices.service : [];
-                promises.push(
-                    new TimeSlot({
-                        from,
-                        location,
-                        date: midnight,
-                        to,
-                        unavailable,
-                        services
-                    }).save()
-                );
-            }
-            let reserve = await appointmentService.getReserveFiltered({
-                date,
-                organization: clinicId,
-                location
-            });
-            reserve = reserve || {};
-            reserve = !!reserve.slot ? reserve.slot : [];
-            for (let slot of reserve) {
-                if ('to' in slot.timePeriod === false) continue;
-                let from = new Date(`${date}T${slot.timePeriod.from}`);
-                let fromNum = from.valueOf();
-                froms.push(fromNum);
-                if (existing.indexOf(fromNum) < 0) {
-                    existing.push(fromNum);
-                    promises.push(
-                        new TimeSlot({
-                            location,
-                            date: midnight,
-                            from,
-                            to: new Date(`${date}T${slot.timePeriod.to}`),
-                            status: slot.status
-                        }).save()
-                    );
-                } else {
-                    promises.push(
-                        TimeSlot.update({
-                            from,
-                            location,
-                            date: midnight
-                        }, {
-                            $set: {
-                                status: slot.status
-                            }
-                        }).exec()
-                    );
-                }
-            }
-            promises.push(
-                TimeSlot.remove({
-                    status: 1,
-                    from: {
-                        $nin: froms
-                    },
-                    location,
-                    date: midnight
-                }).exec()
-            );
+            await update(date, loc, clinicId, appointmentService);
         }
     }
-    await Promise.all(promises);
 };
