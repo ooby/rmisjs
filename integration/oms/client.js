766653c49d53a1165d16183838c29a8c11a7afcb
Afanasy Argunov <afanasy@argunov.com>
2018-01-30 13:36:09 +0900

    fix

--- integration/oms/client.js
+++ integration/oms/client.js
@@ -1,10 +1,9 @@
 const soap = require('soap');
 
-const opts = {
-    endpoint: 'http://10.80.18.60:9765/services/PoiskERZ?wsdl'
-};
-
 module.exports = s => new Promise((resolve, reject) => {
+    const opts = {
+        endpoint: s.oms.path
+    };
     soap.createClient(s.oms.path, opts, (e, c) => {
         if (e) reject(e);
         resolve(c);
