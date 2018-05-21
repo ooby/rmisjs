d1efc5538ce8224bc55cacbb5f25a228e5f86a13
raviolin <raviolin@icloud.com>
2018-05-21 10:47:33 +0900

    0.5.6

--- composer/mongo/sync/appcache.js
+++ composer/mongo/sync/appcache.js
@@ -1,5 +1,17 @@
 const TimeSlot = require('../model/timeslot');
 const mongoose = require('mongoose');
 
-module.exports = s =>
-    TimeSlot.appCache(s.sources).out('appcaches').exec();
+module.exports = async s => {
+    await mongoose
+        .connection
+        .collection('appcaches')
+        .remove({
+            'times.from': {
+                $lte: new Date()
+            }
+        });
+    await TimeSlot
+        .appCache(s.sources)
+        .out('appcaches')
+        .exec();
+};
