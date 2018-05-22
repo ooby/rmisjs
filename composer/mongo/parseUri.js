4a6247c767fcc54fab257cd0fb864a5e41026f1c
raviolin <raviolin@icloud.com>
2018-05-22 11:16:06 +0900

    0.5.9

--- composer/mongo/parseUri.js
+++ composer/mongo/parseUri.js
@@ -1,6 +1,6 @@
 module.exports = config => {
-    if (config.mongo.host) return config.mongo.host;
     let opts = config.mongo.mongoose;
+    if (!opts) return config.mongo.host;
     let result = 'mongodb://';
     if ('username' in opts && 'password' in opts) {
         result += `${opts.username}:${opts.password}@`;
